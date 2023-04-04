dd648d1dcbde183f866d7277268693e7caecb121
andreyusmanov <andreyusmanov@mail.ru>
2023-04-04 16:48:54 +0500

    с2

--- src/main/java/uz/uat/mro/apps/utils/DateUtil.java
+++ src/main/java/uz/uat/mro/apps/utils/DateUtil.java
@@ -1,6 +1,8 @@
 package uz.uat.mro.apps.utils;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import uz.uat.mro.apps.model.common.entity.WorkDate;
 import uz.uat.mro.apps.model.common.repository.WorkDateRepository;
@@ -8,7 +10,7 @@ import uz.uat.mro.apps.model.common.service.WorkDateService;
 
 public class DateUtil {
 
-    private WorkDateService service;
+    private static WorkDateService service;
 
     /**
      * @param repo
@@ -17,16 +19,20 @@ public class DateUtil {
         this.service = service;
     }
 
-public static void main(String[] args) {
-int i = 800;
-LocalDate start = LocalDate.of(2022, 9, 30);
+    public static void main(String[] args) {
 
-for (int j = 0; j < i; j++) {
-    WorkDate w = new WorkDate();
-    w.setId(start.plusDays(j).toString());
-}
+        DateUtil util = new DateUtil(service);
+        int i = 800;
+        LocalDate start = LocalDate.of(2022, 9, 30);
+        List<WorkDate> list = new ArrayList<>();
+        for (int j = 0; j < i; j++) {
+            WorkDate w = new WorkDate();
+            w.setId(start.plusDays(j).toString());
+            w.setDate(start.plusDays(j).toString());
+            list.add(w);
+        }
+        util.service.saveAll(list);
 
-}
-    
+    }
 
 }
