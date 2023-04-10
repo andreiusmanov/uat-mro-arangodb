9d2d03dda48a7487b1d0538dea86e92913604bc1
usmanov1970 <usmanov.ivoice@outlook.com>
2023-04-10 16:46:27 +0500

    c1

--- src/main/java/uz/uat/mro/apps/views/ppcd/views/JobcardsView.java
+++ src/main/java/uz/uat/mro/apps/views/ppcd/views/JobcardsView.java
@@ -1,9 +1,9 @@
 package uz.uat.mro.apps.views.ppcd.views;
 
-import org.vaadin.crudui.layout.impl.VerticalCrudLayout;
-
 import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.menubar.MenuBarVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -13,19 +13,37 @@ import uz.uat.mro.apps.views.ppcd.layouts.PlanningLayout;
 @Route(value = "ppcd/jobcards", layout = PlanningLayout.class)
 public class JobcardsView extends VerticalLayout {
     private MenuBar menu;
+    private MenuBar actionsMenu;
+    private TextField searchField;
 
     /**
      * 
      */
     public JobcardsView() {
         menu();
+        actionsMenu();
+        searchField();
+        add(menu, searchField, actionsMenu);
+    }
+
+    private void actionsMenu() {
+        this.actionsMenu = new MenuBar();
+        actionsMenu.addThemeVariants(MenuBarVariant.LUMO_TERTIARY);
+        actionsMenu.addItem("Открыть");
+        actionsMenu.addItem("Отложить");
+        actionsMenu.addItem("Закрыть");
+    }
+
+    private void searchField() {
+        this.searchField = new TextField("", "", "Поиск");
     }
 
     private void menu() {
-        menu.addItem("Открытые карты");
-        menu.addItem("Отложенные карты");
-        menu.addItem("Закрытые карты");
-
+        this.menu = new MenuBar();
+        menu.addThemeVariants(MenuBarVariant.LUMO_TERTIARY);
+        menu.addItem("Рабочие карты");
+        menu.addItem("Non-routine карты");
+        menu.addItem("EO карты");
     }
 
 }
